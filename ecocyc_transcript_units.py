b624ae61f94a1468b8804df37d9b34f56d7b1493
scwatts <s.watts2@student.unimelb.edu.au>
2016-08-26 00:11:56 +1000

    Added some comments

--- ecocyc_transcript_units.py
+++ ecocyc_transcript_units.py
@@ -3,11 +3,10 @@
 # This script takes a file of EcoCyc gene names and returns the transcript gene
 # units (with assocaited promoter) for each gene
 
-# We're going to have to do this in two parts:
+# We're going to have to do this in three parts:
 #   1. Convert gene names to accessions via API search
-#   2. Make HTTP requests to API to grab common-name for transcript units and promoter
-#       2a. If common-name for promoter does not exist, make another request
-#           for the promoter XML object and extract from there
+#   2. Make HTTP requests to API to grab transcript unit accessions
+#   3. Enumerate transcript unit promoters via API request
 
 
 import argparse
@@ -58,10 +57,13 @@ def main():
         gene_search_xml = et.fromstring(gene_search_response.text)
         gene_tag = gene_search_xml.find('Gene')
 
+        # Sometimes a gene won't be found in the ecoli database (it may be
+        # present in anther however). Either way, if not found we skipped
         if not gene_tag:
             print(gene, 'no_gene_found_in_ecoli_db')
             continue
 
+        # Get the gene accession
         gene_accession = gene_tag.get('ID')
 
 
@@ -74,29 +76,34 @@ def main():
 
         # Extract desired parts from XML
         for ts in ts_unit_xml.findall('Transcription-Unit'):
-
-
+            # Collect transcript unit's genes
             unit_genes_tag = ts.find('common-name')
+            # Sometimes the XML response object does not contain a common name,
+            # so we set it to the gene name
             if not unit_genes_tag:
                 unit_genes = gene
             else:
                 unit_genes = unit_genes_tag.text
 
 
+            # Find the transcript unit accession
             ts_accession = ts.get('ID')
-
-
             # Get transcript unit promoter xml
             ts_promoters_response = requests.get(TS_PROMOTOR_URL % ts_accession)
             ts_promoter_xml = et.fromstring(ts_promoters_response.text)
 
 
+            ####
+            # Part 3: Get transcript unit promoters
+            ###
             ts_promoters = ts_promoter_xml.findall('Promoter/common-name')
 
+            # If transcript unit has no promoter, skip
             if not ts_promoters:
                 print(gene, 'no_promoter_with_evidence')
                 continue
 
+            # Print out transcript unit's promoter
             for ts_promoter in ts_promoters:
                 promoter_name = ts_promoter.text
                 print(gene, promoter_name, unit_genes, sep='\t', flush=True)
